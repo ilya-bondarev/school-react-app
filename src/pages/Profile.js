a666ff6a5a13bed35a69f3184efd475933c1b720
IlyaB <my.own.securyti@protonmail.com>
2024-06-10 17:27:28 +0300

    Design improvement

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -22,21 +22,14 @@ const Profile = () => {
             <div className="profile-details">
                 <img src={profile.photo} alt="Profile" style={{ maxWidth: 50 + '%' }} />
                 <p><strong>Имя:</strong> {profile.full_name}</p>
-                <p><strong>Роль:</strong> {profile.role_id}</p>
+                <p><strong>Роль:</strong> 
+                    {profile.role_id === 2 && (<span> Учитель</span>)}
+                    {profile.role_id === 3 && (<span> Ученик</span>)}
+                </p>
                 <p><strong>Логин:</strong> {profile.login}</p>
                 <p><strong>Дата регистрации:</strong> {new Date(profile.registration_date).toLocaleDateString()}</p>
                 <p><strong>Описание:</strong> {profile.description}</p>
             </div>
-            {profile.role_id === 2 && (
-                <div>
-                    Профиль учителя
-                </div>
-            )}
-            {profile.role_id === 3 && (
-                <div>
-                    Профиль ученика
-                </div>
-            )}
             <div className="profile-actions">
                 <button className="btn change-profile-btn" onClick={handleProfileChange}>Изменить профиль</button>
                 <button className="btn logout-btn" onClick={handleLogout}>Выход</button>
